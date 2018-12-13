7151cdaaef4a830ce515639829b283bd125523e4
Noemie Gavalda <gavald_n@epita.fr>
2018-12-13 23:51:56 +0100

    etape 1 et 2

--- src/components/FavouriteToggle.js
+++ src/components/FavouriteToggle.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
 
-const FavouriteToggle = ({ isActive = false, style = {} }) => (
-  <span style={style}>{isActive ? '♥️' : '♡'}</span>
-);
+const FavouriteToggle = ({ isActive = false, style = {} }) => {
+  const [fav, setFav] = useState(isActive);
+  return (
+    <span onClick={() => setFav(!fav)} style={style}>{fav ? '♥️' : '♡'}</span>
+  )
+};
 
 export default FavouriteToggle;
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import FavouriteToggle from './FavouriteToggle';
+import { useState } from 'react';
 
-const Post = ({ data }) => (
+const Post = ({ data }) => {
+  const [body, setBody] = useState('');
+  return (
   <li
     style={{
       backgroundColor: 'white',
@@ -11,11 +14,22 @@ const Post = ({ data }) => (
       fontSize: '11pt',
       cursor: 'pointer'
     }}
+    
   >
-    <span style={{ fontWeight: '900' }}>{data.title}</span>
-
+    <span 
+      style={{ fontWeight: '900' }} 
+      onClick={() => {
+        if (body === '')
+          setBody(data.body)
+        else
+          setBody('')
+    }}>{data.title}</span>
     <FavouriteToggle style={{ float: 'right' }} />
+    <div style={{ fontWeight: '900' }}>{body}</div>     
+
+    
   </li>
-);
+  )
+};
 
 export default Post;
